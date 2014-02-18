600712bda0d6d753b936cba2b95be71288c4fea5
yusef <yusef.ali.garcia@gmail.com>
2014-02-18 12:31:48 +0100

    phantomjs

--- config/routes.js
+++ config/routes.js
@@ -95,8 +95,38 @@ module.exports = function(app, passport, auth) {
     
 
     //Home route
+    var phantom = require('node-phantom');
     var index = require('../app/controllers/index');
    // app.get('/', index.render);
-    app.get('/', index.render);
+    // app.get('/', index.render);
 
+	app.get('/', function(req, res){ 
+		// If there is _escaped_fragment_ option, it means we have to // generate the static HTML that should normally return the Javascript 
+		if(typeof(req.query._escaped_fragment_) !== "undefined") { 
+			console.log("_escaped_fragment_");
+			phantom.create(function(err, ph) { 
+				return ph.createPage(function(err, page) { 
+					// We open phantomJS at the proper page.
+					console.log("createPage"); 
+					return page.open("http://valeporun.com/#!" + req.query._escaped_fragment_, function(status) {
+						console.log("createPage FIN");  
+						return page.evaluate((function() { 
+							console.log("page.evaluate"); 
+							// We grab the content inside <html> tag... 
+							return document.getElementsByTagName('html')[0].innerHTML; }), function(err, result) {
+								console.log("page.evaluate FIN");  
+								// ... and we send it to the client. 
+								res.send(result); 
+								return ph.exit(); 
+							});
+						});
+					});
+				});
+		} else 
+		{
+			console.log("index");		
+			// If there is no _escaped_fragment_, we return the normal index template. 
+			res.render('index');
+		}
+	});
 };
